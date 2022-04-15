4aff0f75418965f50e941afe9e739544a787254e
ici806 <evergreen0208@hanmail.net>
2022-04-15 10:40:33 +0900

    파일 읽기, 쓰기

---
+++ app/src/main/java/com/cookandroid/project8_1/MainActivity.java
@@ -0,0 +1,41 @@
+package com.cookandroid.project8_1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
+
+import java.util.Calendar;
+
+public class MainActivity extends AppCompatActivity {
+
+    DatePicker dp;
+    EditText edtDiary;
+    Button btnWrite;
+    String filename;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        setTitle("간단한 일기장");
+
+        dp = (DatePicker) findViewById(R.id.datePicker1);
+        edtDiary = (EditText) findViewById(R.id.edtDiary);
+        btnWrite = (Button) findViewById(R.id.btnWrite);
+
+        Calendar cal = Calendar.getInstance();
+        int cYear = cal.get(Calendar.YEAR);
+        int cMonth = cal.get(Calendar.MONTH);
+        int cDay = cal.get(Calendar.DAY_OF_MONTH);
+
+        dp.init(cYear, cMonth, cDay, new DatePicker.OnDateChangedListener() {
+            @Override
+            public void onDateChanged(DatePicker datePicker, int i, int i1, int i2) {
+
+            }
+        });
+    }
+}
